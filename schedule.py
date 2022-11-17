ec020817bd913beffd45d7c5d33a3a873f5bd2bf
Ketaha <67551749+Ketaha@users.noreply.github.com>
2022-11-17 18:04:38 +0200

    Final commit

--- schedule.py
+++ schedule.py
@@ -1,13 +1,23 @@
 from time import sleep
-from webbrowser import open
 from datetime import date
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains as AC
 
 weekNumber = date.today().isocalendar().week
+todayDate = date.today().day
 
 # Checks if day of request is EOW; if is makes request with next week
 weekDay = date.today().isoweekday()
 if weekDay >= 6:
     weekNumber+=1
 
-open("https://nvna.eu/wp/?group=128211&queryType=group&Week=%s" % str(weekNumber))
-sleep(1)
+driver = webdriver.Edge()
+driver.get("https://nvna.eu/wp/?group=128211&queryType=group&Week=%s" % str(weekNumber))
+
+dayName = ['Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота', 'Неделя']
+element = driver.find_element(By.XPATH, "//*[contains(text(),'%s')]" % dayName[weekDay])
+actions = AC(driver)
+actions.scroll_by_amount(0, element.location['y']).perform()
+
+sleep(10)
