2b1a30b078d5b4b8f0767a8a9a1939c3c487802c
Ketaha <67551749+Ketaha@users.noreply.github.com>
2022-12-01 01:15:18 +0200

    Add files via upload

--- schedule.py
+++ schedule.py
@@ -1,8 +1,15 @@
+import selenium.webdriver as WD
 from time import sleep
 from datetime import date
+from argparse import ArgumentParser
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains as AC
+
+# CLI arguments
+parser = ArgumentParser(description='Check schedule')
+parser.add_argument('cg', type=str, help='number of group to test the schedule for')
+parser.add_argument('-t', '--tomorrow', action='store_true', help='displays schedule for tomorrow')
+args = parser.parse_args()
 
 weekNumber = date.today().isocalendar().week
 
@@ -10,13 +17,14 @@ weekNumber = date.today().isocalendar().week
 weekDay = date.today().isoweekday()
 if weekDay >= 6:
     weekNumber+=1
+    weekDay = 1
+dayName = ['Неделя', 'Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота']
 
-driver = webdriver.Edge()
-driver.get("https://nvna.eu/wp/?group=128211&queryType=group&Week=%s" % str(weekNumber))
+weekDay += 1 if args.tomorrow == True else None
 
-dayName = ['Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота', 'Неделя']
+driver = WD.Edge()
+driver.get("https://nvna.eu/wp/?group=%s&queryType=group&Week=%s" % (args.cg, str(weekNumber)))
 element = driver.find_element(By.XPATH, "//*[contains(text(),'%s')]" % dayName[weekDay])
-actions = AC(driver)
-actions.scroll_by_amount(0, element.location['y']).perform()
+AC(driver).scroll_by_amount(0, element.location['y']).perform()
 
-sleep(10)
+sleep(7)
